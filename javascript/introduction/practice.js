3a9373b6a33547b01e5055a0c989d1caa3c46b01
ramiturkmani <ramiturkmani1@gmail.com>
2023-07-06 23:10:52 -0500

    slight format revisions

--- javascript/introduction/practice.js
+++ javascript/introduction/practice.js
@@ -10,24 +10,24 @@ console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${spous
 //The Age Calculator
 var birthYear = 1932;
 var futureYear = 2078;
-var age = futureYear-birthYear;
+var age = futureYear - birthYear;
 
-console.log(`I will be either ${age-1} or ${age} in ${futureYear}`);
+console.log(`I will be either ${age - 1} or ${age} in ${futureYear}`);
 
 
 //The Lifetime Supply Calculator
 var currentAge = 15;
 var maxAge = 85;
 var dailyAmount = 6;
-var totalAmount = ((maxAge-currentAge)*365)*6;
+var totalAmount = ((maxAge - currentAge) * 365) * 6;
 
 console.log(`You will need ${totalAmount} to last you until the ripe old age of ${maxAge}.`)
 
 
 //The Geometrizer
 var radius = 15;
-var circumference = 2*Math.PI*radius;
-var area = Math.PI*(Math.pow(radius,2));
+var circumference = 2 * Math.PI * radius;
+var area = Math.PI * (Math.pow(radius, 2));
 
 console.log(`The circumference is ${circumference}`);
 console.log(`The area is ${area}`);
@@ -35,9 +35,9 @@ console.log(`The area is ${area}`);
 
 //The Temperature Converter
 var tempC = 15;
-var convertedTempC = (tempC * (9/5)) + 32;
+var convertedTempC = (tempC * (9 / 5)) + 32;
 var tempF = 99;
-var convertedTempF = (tempF - 32) * (5/9);
+var convertedTempF = (tempF - 32) * (5 / 9);
 
 console.log(`${tempC}°C is ${convertedTempC}°F`);
 console.log(`${tempF}°F is ${convertedTempF}°C`);
